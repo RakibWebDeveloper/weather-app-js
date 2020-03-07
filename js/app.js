4dc288eb4455d675bcf2df39d0e918f2b80e485a
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-03-07 12:18:15 +0600

    Added city name and country

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Set current date and time
-
 var currentDate = new Date();
-
 var dateElement = document.getElementById("date");
 dateElement.innerText = currentDate.toDateString();
 
+
 // fetch city and country from ip api
+// Select city and country
+
+
+
 
 async function getCity() {
     const city = await fetch('https://ipapi.co/json');
@@ -13,4 +16,9 @@ async function getCity() {
     return data;
 }
 
-console.log(getCity());
+getCity().then(data => {
+    // city.innerText = data.city;
+    // country.innerText = data.country;
+    console.log(data.city);
+    
+});
---
+++ js/ui.js
@@ -0,0 +1,11 @@
+class UI {
+    constructor() {
+        this.city = document.getElementById("city");
+        this.country = document.getElementById("country");
+        this.desc = document.getElementsByClassName("desc");
+        this.icon_desc = document.getElementById("icon-desc");
+        this.humidity=document.getElementById("humidity");
+        this.wind=document.getElementById("wind");
+        this.visibility=document.getElementById("visibility");
+    }
+}
